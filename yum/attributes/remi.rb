a3991761281471c9e468e4b134e31147ba99b6c7
root <root@caper-017.hq.couchbase.com>
2013-03-27 16:26:40 -0700

    Import yum version 2.2.0

--- yum/attributes/remi.rb
+++ yum/attributes/remi.rb
@@ -1,8 +1,7 @@
 #
 # Cookbook Name:: yum
-# Recipe:: yum 
+# Attributes:: remi
 #
-# Copyright 2011, Eric G. Wolfe
 # Copyright 2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +17,14 @@
 # limitations under the License.
 #
 
-template "/etc/yum.conf" do
-  source "yum-rhel#{node['platform_version'].to_i}.conf.erb"
+case node['platform']
+when "fedora"
+  default['yum']['remi']['url'] = "http://rpms.famillecollet.com/fedora/#{node['platform_version'].to_i}/remi/mirror"
+else
+  default['yum']['remi']['url'] = "http://rpms.famillecollet.com/enterprise/#{node['platform_version'].to_i}/remi/mirror"
 end
+
+default['yum']['remi']['key'] = "RPM-GPG-KEY-remi"
+default['yum']['remi']['key_url'] = "http://rpms.famillecollet.com/#{node['yum']['remi']['key']}"
+default['yum']['remi']['includepkgs'] = nil
+default['yum']['remi']['exclude'] = nil
