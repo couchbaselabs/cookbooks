cd2652061d5b15f218f03614b227a8ffcaeb5f2d
root <root@caper-017.hq.couchbase.com>
2013-04-16 15:49:45 -0700

    New pip.rb changes

--- python/providers/pip.rb
+++ python/providers/pip.rb
@@ -109,7 +109,6 @@ def current_installed_version
     end
     p = shell_out!(version_check_cmd)
     p.stdout.split(delimeter)[1].strip
-  #rescue Mixlib::ShellOut::ShellCommandFailed
   rescue Chef::Exceptions::ShellCommandFailed, Mixlib::ShellOut::ShellCommandFailed
   end
 end
